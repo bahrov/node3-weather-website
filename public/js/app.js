42f40f391ae601b876ab3ddcfbacd6e20bba2bc7
Maksym Bahrov <maksymbahrov@Maksyms-MacBook-Pro.local>
2022-03-02 22:33:34 +0200

    Set up app for haroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,17 +7,15 @@ weatherForm.addEventListener('submit', (event) => {
   event.preventDefault();
   message1.textContent = 'Loading...';
   message2.textContent = '';
-  fetch('http://localhost:3000/weather?address=' + searchData.value).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          message1.textContent = data.error;
-          message2.textContent = '';
-        } else {
-          message1.textContent = '';
-          message2.textContent = `In ${data.location} is ${data.forecast.description}. It is ${data.forecast.temperature}, and it feels like ${data.forecast.feelsLike}`;
-        }
-      });
-    }
-  );
+  fetch('/weather?address=' + searchData.value).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        message1.textContent = data.error;
+        message2.textContent = '';
+      } else {
+        message1.textContent = '';
+        message2.textContent = `In ${data.location} is ${data.forecast.description}. It is ${data.forecast.temperature}, and it feels like ${data.forecast.feelsLike}`;
+      }
+    });
+  });
 });
